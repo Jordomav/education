f3f4144f50bad69fb955f6cae86a112a4806e1c7
Jordan Mavrogeorge <jordomav@gmail.com>
2018-02-16 13:29:12 -0800

    ready for redeploy

--- src/app/mock-courses.ts
+++ src/app/mock-courses.ts
@@ -1,7 +1,7 @@
 import { Course } from './course';
 
 export const COURSES: Course[] = [
-  {id: 1, name: 'Websites for Beginners', desc: 'Learn to build a basic one page website using HTML and CSS principles.', color: '#e34f26', tags: ['HTML', 'CSS'], route: '/courses/websitesforbeginners/1', status: true},
+  {id: 1, name: 'Websites for Beginners', desc: 'Learn to build a basic one page website using HTML and CSS principles.', color: '#e34f26', tags: ['HTML', 'CSS'], route: '/courses/websitesforbeginners/1', status: false},
   {id: 2, name: 'Introduction to App Development', desc: 'Make a basic 3 page application using the Ionic Framework.', color: '#488aff', tags: ['HTML', 'CSS', 'Ionic', 'Apps', 'TypeScript'], route: '/courses/introionic/1', status: false},
   // {id: 3, name: 'Introduction to Node.js', desc: 'Learn to make a basic server using Node.js and Express.', color: '#215732', tags: ['JavaScript', 'Node', 'Express'], route: '/courses/intronode/1', status: false},
   // {id: 4, name: 'Introduction to PHP', desc: 'Learn the basics of PHP.', color: '#4f5b93', tags: ['PHP'], route: '/courses/introphp/1', status: false},
