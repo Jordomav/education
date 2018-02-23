9e950840fef6b41985f49840707cd440923b009b
Jordan Mavrogeorge <jordomav@gmail.com>
2018-02-22 23:12:31 -0800

    added codeigniter

--- src/app/mock-courses.ts
+++ src/app/mock-courses.ts
@@ -1,14 +1,15 @@
 import { Course } from './course';
 
 export const COURSES: Course[] = [
-  {id: 1, name: 'Websites for Beginners', desc: 'Learn to build a basic one page website using HTML and CSS principles.', color: '#e34f26', tags: ['HTML', 'CSS'], route: '/courses/websitesforbeginners/1', status: true},
+  {id: 1, name: 'Websites for Beginners', desc: 'Learn to build a basic one page website using HTML and CSS principles.', color: '#e34f26', tags: ['HTML', 'CSS'], route: '/courses/websitesforbeginners/1', status: false},
   {id: 2, name: 'Introduction to App Development', desc: 'Make a basic 3 page application using the Ionic Framework.', color: '#488aff', tags: ['HTML', 'CSS', 'Ionic', 'Apps', 'TypeScript'], route: '/courses/introionic/1', status: false},
-  {id: 3, name: 'Introduction to Node.js', desc: 'Learn to make a basic server using Node.js and Express.', color: '#215732', tags: ['JavaScript', 'Node', 'Express'], route: '/courses/intronode/1', status: false},
-  {id: 4, name: 'Introduction to PHP', desc: 'Learn the basics of PHP.', color: '#4f5b93', tags: ['PHP'], route: '/courses/introphp/1', status: false},
-  {id: 5, name: 'Introduction to Unity', desc: 'Create an old school side scrolling game.', color: '#222c37', tags: ['Unity', 'Games', 'C#'], route: '/courses/introunity/1', status: false},
-  {id: 6, name: 'Introduction to Laravel', desc: 'Build stunningly beautiful web apps in PHP', color: '#f55247', tags: ['php', 'laravel'], route: '/courses/introlaravel/1', status: false},
-  {id: 7, name: 'Introduction to Angular.js', desc: 'Create amazing web apps with HTML, CSS and TypeScript', color: '#b52e31', tags: ['Angular', 'TypeScript'], route: '/courses/introangular/1', status: false},
+  // {id: 3, name: 'Introduction to Node.js', desc: 'Learn to make a basic server using Node.js and Express.', color: '#215732', tags: ['JavaScript', 'Node', 'Express'], route: '/courses/intronode/1', status: false},
+  // {id: 4, name: 'Introduction to PHP', desc: 'Learn the basics of PHP.', color: '#4f5b93', tags: ['PHP'], route: '/courses/introphp/1', status: false},
+  // {id: 5, name: 'Introduction to Unity', desc: 'Create an old school side scrolling game.', color: '#222c37', tags: ['Unity', 'Games', 'C#'], route: '/courses/introunity/1', status: false},
+  // {id: 6, name: 'Introduction to Laravel', desc: 'Build stunningly beautiful web apps in PHP', color: '#f55247', tags: ['php', 'laravel'], route: '/courses/introlaravel/1', status: false},
+  // {id: 7, name: 'Introduction to Angular.js', desc: 'Create amazing web apps with HTML, CSS and TypeScript', color: '#b52e31', tags: ['Angular', 'TypeScript'], route: '/courses/introangular/1', status: false},
   {id: 8, name: 'Introduction to Amazon Alexa', desc: 'Create your own Alexa Skills for Americas most popular home assistant', color: '#31C4F3', tags: ['Alexa', 'AI', 'Amazon'], route: '/courses/introalexa/1', status: false},
-  {id: 9, name: 'Introduction to Electron.js', desc: 'Build native desktop apps with JavaScript, HTML and CSS', color: '#74b1be', tags: ['Electron', 'Desktop Apps'], route: '/courses/introelectron/1', status: false},
-  {id: 10, name: 'Robotics with the Edison robot', desc: 'Learn basic robotics with the Edison Robot.', color: '#ff7000', tags: ['Robotics', 'Edison', 'python'], route: '/courses/edison/1', status: false}
+  // {id: 9, name: 'Introduction to Electron.js', desc: 'Build native desktop apps with JavaScript, HTML and CSS', color: '#74b1be', tags: ['Electron', 'Desktop Apps'], route: '/courses/introelectron/1', status: false},
+  {id: 10, name: 'Robotics with the Edison robot', desc: 'Learn basic robotics with the Edison Robot.', color: '#ff7000', tags: ['Robotics', 'Edison', 'python'], route: '/courses/edison/1', status: false},
+  {id: 11, name: 'Introduction to CodeIgniter and MySQL', desc: 'Make web apps with PHP and the CodeIngiter Framework.', color: '#dd4814', tags: ['PHP', 'MySQL', 'CodeIngiter'], route: '/courses/codeingiter/1', status: false}
 ];
